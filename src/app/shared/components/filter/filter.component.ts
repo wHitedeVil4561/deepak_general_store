195cc1d30fe69cc47037d5c603c61b96c9c01a6d
Deepak-Kumar-4561 <deepak.gupta@appventurez.com>
2022-11-27 23:04:57 +0530

    filter

--- src/app/shared/components/filter/filter.component.ts
+++ src/app/shared/components/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FilterComponent implements OnInit {
 
   constructor() { }
-
+  @Input() filteroptions:any;
   ngOnInit(): void {
   }
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { DeleteModalComponent } from './components/delete-modal/delete-modal.component';
 import { ConfirmationModalComponent } from './components/confirmation-modal/confirmation-modal.component';
 import { FilterComponent } from './components/filter/filter.component';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { FilterComponent } from './components/filter/filter.component';
     ConfirmationModalComponent,
     FilterComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule,NgSelectModule],
   exports: [
     HeaderComponent,
     DeleteModalComponent,
